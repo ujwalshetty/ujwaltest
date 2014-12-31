08071dcbac7bd843a94679c9152ede71ddec01b7
ujwalshetty <ujwal.shettygunner@gmail.com>
2014-12-31 15:20:54 +0530

    updated spring project

---
+++ springproject/src/com/prognosys/Test.java
@@ -0,0 +1,16 @@
+package com.prognosys;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("springConfig.xml");
+	Object e=context.getBean("prognosys");
+	
+	
+
+	}
+
+}
